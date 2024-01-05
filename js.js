7507b5431a47d8ca004136762c9e438fc3e22a66
cedywade <149687844+cedywade@users.noreply.github.com>
2024-01-05 12:31:09 -0600

    new page 2024
    new page i was in wrong page updated!

---
+++ js.js
@@ -0,0 +1,6 @@
+grabSubmitBtn.addEventListener("click"), (e) => {
+    e.preventDefault();
+    console.log(SearchInput.value);
+}
+const SearchInput = document.getElementById("Search");
+console.log(SearchInput);
